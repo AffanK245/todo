779cb5125ac4babbcfa92e51827482316d7a9d27
Affan <affankarikazi121@gmail.com>
2025-09-08 22:15:49 +0530

    New changes are made

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-# todo.py - simple TodoList example
+# todo.py - simple TodoList example with list function
 
 class TodoList:
     def __init__(self):
@@ -11,11 +11,15 @@ class TodoList:
         if 0 <= idx < len(self.items):
             self.items.pop(idx)
 
-    def __str__(self):
+    def list_tasks(self):
+        if not self.items:
+            return "No tasks yet!"
         return "\n".join(f"{i+1}. {t}" for i, t in enumerate(self.items))
 
 if __name__ == "__main__":
     t = TodoList()
     t.add("Buy milk")
     t.add("Learn Git")
-    print(t)
+    t.add("Practice Python")
+    print("My tasks:")
+    print(t.list_tasks())
